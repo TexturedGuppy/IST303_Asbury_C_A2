fe31393ad03ee2cc7e7bcef4b5504f0015616d0c
Cameron Asbury <asbury56@marshall.edu>
2016-09-14 10:30:33 -0400

    Work on Item class
    Added main constructor

--- IST303_Asbury_C_A2/Item.cs
+++ IST303_Asbury_C_A2/Item.cs
@@ -16,7 +16,16 @@ namespace IST303_Asbury_C_A2
         //Default Constructor
         public Item()
         {
+            iName = "Unnamed Item";
+            numItems = 0;
+            price = 0;
+        }
 
+        public Item(string _iName, int _numItems, double _price)
+        {
+            iName = _iName;
+            numItems = _numItems;
+            price = _price;
         }
     }
 }
